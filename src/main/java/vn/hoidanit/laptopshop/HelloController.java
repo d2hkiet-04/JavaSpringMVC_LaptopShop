d9ae92666775429d51299c719180afb4fae3582f
DoDiHoKi <doankiet078@gmail.com>
2024-11-04 21:15:31 +0700

    connect database and spring securitysecurity

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hoi Dan IT!";
+        return "Hello World with Doan Dinh Hoang Kiet!";
+    }
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only User can access this page!";
+    }
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin can access this page!";
     }
 }
